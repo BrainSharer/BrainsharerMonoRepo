78188fa9a044d7a3edd7a133ba6d9f0e2c6af8b6
Edward O'Donnell <eddy.odonnell@gmail.com>
2024-06-25 08:57:26 +0800

    Fixed sql

--- django_api/sql/2024-06-23.updates.sql
+++ django_api/sql/2024-06-23.updates.sql
@@ -20,11 +20,4 @@ group by as2.annotation_type, as2.FK_prep_id,  br.abbreviation, au.username
 having count(*) > 1
 order by as2.annotation_type, as2.FK_prep_id,  br.abbreviation, au.username);
 
-select br.abbreviation , as2.FK_prep_id , au.username 
-from annotation_session as2 
-inner join brain_region br on as2.FK_brain_region_id = br.id
-inner join auth_user au on as2.FK_user_id = au.id
-where as2.id = 7398
-
-and active=1;
 
